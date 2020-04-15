5c4e95a91c445b87f5b31599b0dcdbbcac3d7dbc
Vlad <vbondarenko1@sheffield.ac.uk>
2020-04-15 13:27:43 +0100

    Added regional support, changed r_sqr to return a single value

--- validation.py
+++ validation.py
@@ -16,54 +16,70 @@ from pvlive_api import PVLive
 from helper import shift_n_days, load_from_file, to_utc
 
 class Validation:
+
     def __init__(self, options=None):
         if options is None:
             self.options = self.parse_options()
         else:
             self.options = options
 
-    def run_validation(self, forecast=None, min_yield=0.05):
-        data = self.get_data(forecast, min_yield)
-        horizons = data.index.get_level_values(1)
-        dt = data.index.get_level_values(0)
-        display_data = dict()
-        display_data['data'] = dict()
-        display_data['data']['Region0'] = dict()
-        display_data['data']['Region0']['intraday'] = dict()
-        display_data['data']['Region0']['day1'] = dict()
-        for base in dt.hour.unique():
-            base_str = str(time(hour=base))[:5]
-            intraday = data[(dt.hour == base) & (horizons.isin(np.arange(0, 48 - base)))]
-            dayp1 = data[(dt.hour == base) & (horizons.isin(np.arange(48 - (base * 2), 96 - (base * 2))))]
-            preiod_names = ['intraday', 'day1']
-            for i, period_data in enumerate([intraday, dayp1]):
-                display_data['data']['Region0'][preiod_names[i]][base_str] = dict()
-                display_period = display_data['data']['Region0'][preiod_names[i]][base_str]
-                pred, actual, cap = period_data['forecast'], period_data['actual'], period_data['cap']
-                pred_u, actual_u, cap_u = pred.unstack(), actual.unstack(), cap.unstack()
-                errors = pd.DataFrame(index=pred_u.index)
-                errors['mape'] = self.wmape(pred_u, actual_u, cap_u, axis=1)
-                errors['r_squared'] = self.r_squared(pred_u, actual_u)
-                display_period['r_squared'] = errors['r_squared'].values.tolist()
-                display_period['mape'] = errors['mape'].values.tolist()
-                display_period['actual'] = actual.values.tolist()
-                display_period['predicted'] = pred.values.tolist()
-                display_period['heatmap'] = self.calc_heatmap(pred, actual, cap)
-        return display_data
+    def run_validation(self, region, forecast=None, min_yield=0.05):
+        region_str = f'Region {region}'
+		data = self.get_data(forecast, min_yield, region)
+
+		horizons = data.index.get_level_values(1)
+		dt = data.index.get_level_values(0)
+
+		display_data = dict()
+		display_data[region_str] = dict()
+		display_data[region_str]['intraday'] = dict()
+		display_data[region_str]['day1'] = dict()
+
+		for base in dt.hour.unique():
+			base_str = str(time(hour=base))[:5]
+
+			intraday = data[(dt.hour == base) & (horizons.isin(np.arange(0, 48 - base)))]
+			dayp1 = data[(dt.hour == base) & (horizons.isin(np.arange(48 - (base * 2), 96 - (base * 2))))]
+
+			preiod_names = ['intraday', 'day1']
+			for i, period_data in enumerate([intraday, dayp1]):
+
+				display_data[region_str][preiod_names[i]][base_str] = dict()
+				display_period = display_data[region_str][preiod_names[i]][base_str]
+
+				pred, actual, cap = period_data['forecast'], period_data['actual'], period_data['cap']
+				pred_u, actual_u, cap_u = pred.unstack(), actual.unstack(), cap.unstack()
+
+				errors = pd.DataFrame(index=pred_u.index)
+				errors['mape'] = self.wmape(pred_u, actual_u, cap_u, axis=1)
+				errors['r_squared'] = self.r_squared(pred, actual)
+
+				display_period['r_squared'] = errors['r_squared']
+				display_period['mape'] = errors['mape'].values.tolist()
+				display_period['actual'] = actual.values.tolist()
+				display_period['predicted'] = pred.values.tolist()
+				display_period['heatmap'] = self.calc_heatmap(pred, actual, cap)
+
+		return display_data
 
     def get_data(self, forecast, min_yield):
+
         if forecast is None:
             forecast = load_from_file(self.options['forecast_file'])[0]
-        forecast = to_utc(forecast)
+
         gen, cap = self.get_pvlive_data(forecast.index)
         gen.dropna(inplace=True)
         forecast = forecast.reindex(gen.index).dropna()
         cap = cap.reindex(gen.index).dropna()
+
         day_values = (gen.values.flatten()/cap.values.flatten()) > min_yield
         df = forecast[day_values]
+        if not isinstance(df, pd.DataFrame):
+			df = df.to_frame()
         df.columns = ['forecast']
-        df = df.assign(actual=gen[day_values])
-        df = df.assign(cap=cap[day_values])
+        df.loc[:, 'actual'] = gen[day_values]
+		df.loc[:, 'cap'] = cap[day_values]
+
         return df
 
     @staticmethod
@@ -79,18 +95,23 @@ class Validation:
             -------
             pv_data
         """
+
         datetimes = forecast_index.get_level_values(0).unique()
         horizons = forecast_index.get_level_values(1).unique()
+
         start = datetimes[0]
         end = datetimes[-1] + timedelta(hours=73)
         pvlive = PVLive()
+
         pvlive_data = pvlive.between(start, end, pes_id=0, extra_fields="installedcapacity_mwp")
         pvlive_df = pd.DataFrame(pvlive_data, columns=['region_id', 'datetime', 'gen', 'cap'])
         pvlive_df.index = pd.to_datetime(pvlive_df['datetime'])
         pvlive_df.drop(columns=['datetime', 'region_id'], inplace=True)
+
         pv_gen = shift_n_days(pvlive_df['gen'].values.reshape(-1, 1), horizons[0], horizons[-1]+1, reverse=True)
         pv_gen = pd.DataFrame(data=pv_gen,
                               index=pd.to_datetime(pvlive_df.index)).drop(columns=0).stack().reindex(forecast_index)
+
         pv_cap = shift_n_days(pvlive_df['cap'].values.reshape(-1, 1), horizons[0], horizons[-1] + 1, reverse=True)
         pv_cap = pd.DataFrame(data=pv_cap,
                               index=pd.to_datetime(pvlive_df.index)).drop(columns=0).stack().reindex(forecast_index)
@@ -124,11 +145,11 @@ class Validation:
         ----------
         .. [1] https://en.wikipedia.org/wiki/Coefficient_of_determination
         """
-        mean_actual = np.mean(actuals, axis=1).values.reshape(-1, 1)
-        ss_tot = np.nansum(np.power(actuals.values - mean_actual, 2), axis=1) + 0.0001
-        ss_res = np.nansum(np.power(actuals.values - predictions.values, 2), axis=1)
-        r_sqr = 1 - ss_res / ss_tot
-        return pd.DataFrame(data=r_sqr, columns=['r2'], index=actuals.index)
+        mean_actual = np.mean(actuals)
+		ss_tot = np.sum(np.power(actuals - mean_actual, 2))
+		ss_res = np.sum(np.power(actuals - predictions, 2))
+		r_sqr = 1 - ss_res / ss_tot
+		return r_sqr
 
     @staticmethod
     def wmape(predictions, actuals, norms=None, weights=None, axis=0):
